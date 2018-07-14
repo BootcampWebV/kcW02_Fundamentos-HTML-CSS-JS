8147835cae2b606e7e06baa98df46068ced3acd3
NauelG <g.nauel@gmail.com>
2018-07-14 22:32:25 +0200

    Pequeños ajustes menu, falta retocar icono que desplega.

--- js/menu.js
+++ js/menu.js
@@ -5,22 +5,47 @@ export class Menu {
         this.mainMenu = document.querySelector('#mainNav')
         this.btnMenu = document.querySelector('#btnNav')
 
-
         this.btnMenu.addEventListener('click', this.showMenu.bind(this))
+
+        this.aMenu = document.querySelectorAll('#mainNav a')
+        this.aMenu.forEach(
+            (element) => {
+                element.addEventListener('click', this.navigate.bind(this))
+            }
+        )
+        console.log(this.btnMenu);
+
 
     }
 
     showMenu(element) {
         element.preventDefault()
-        this.mainMenu.classList.toggle('viewNav');
+        console.log(element)
+        this.mainMenu.classList.toggle('noNav');
 
         // Alteramos el botón que maneja el nav
-        element.target.classList.toggle('dNone')
+        console.log(element)
+            // element.target.classList.toggle('dNone')
 
-        if (element.target.previousElementSibling) {
-            element.target.previousElementSibling.classList.toggle('dNone')
+        if (element.target.classList.contains('fa-bars')) {
+            element.target.classList.replace('fa-bars', 'fa-times')
         } else {
-            element.target.nextElementSibling.classList.toggle('dNone')
+            element.target.classList.replace('fa-times', 'fa-bars')
         }
+        // if (element.target.previousElementSibling) {
+        //     console.log(element.target.previousElementSibling)
+        //     element.target.previousElementSibling.classList.toggle('dNone')
+        // } else {
+        //     element.target.nextElementSibling.classList.toggle('dNone')
+        // }
+    }
+
+    navigate(element) {
+        element.preventDefault()
+        let target = element.target.hash
+        document.querySelector(target).scrollIntoView({
+            behavior: 'smooth'
+        })
+        this.showMenu(element)
     }
 }
